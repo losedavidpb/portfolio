b8e306b4781c8df83f14ea180072a58405910371
losedavidpb <losedavidpb@gmail.com>
2025-07-24 16:52:10 +0100

    add loading spinner and fix language selector

--- source/main.js
+++ source/main.js
@@ -24,8 +24,18 @@ async function load_localization(filename) {
  * Set language selector based on current language
  */
 function set_lang_selector() {
-    let lang_index = lang == 'es' ? 0 : 1;
-    let other_lang_index = lang_index == 0 ? 1 : 0;
+    const curr_page = window.location.pathname.split('/').pop().split('?')[0];
+
+    const urls = {
+        es: `${curr_page}?lang=es`,
+        en: `${curr_page}?lang=en`
+    };
+
+    $("#language-div").children().eq(0).attr("href", urls.es);
+    $("#language-div").children().eq(1).attr("href", urls.en);
+
+    const lang_index = lang === 'es' ? 0 : 1;
+    const other_lang_index = lang_index === 0 ? 1 : 0;
 
     $("#language-div").children().eq(lang_index).addClass("active");
     $("#language-div").children().eq(other_lang_index).removeClass("active");
@@ -35,6 +45,8 @@ function set_lang_selector() {
  * Set navbar links
  */
 function set_navbar_links() {
+    const curr_page = window.location.pathname.split('/').pop().split('?')[0];
+
     $("#index-nav-link").attr("href", "./index.html?lang=" + lang)
     $("#contact-nav-link").attr("href", "./contact.html?lang=" + lang);
 }
@@ -56,6 +68,10 @@ async function main() {
     }
 
     prepare_json(json_data, lang);
+
+    $("#loader").removeClass("show").one("transitionend", function () {
+        $(this).remove();
+    });
 }
 
 main();
